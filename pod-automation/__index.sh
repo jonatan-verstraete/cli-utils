231bb0d76d5b65fb80f3b89849fcf12f18ad8006
verstraete-jonatan <138657778+verstraete-jonatan@users.noreply.github.com>
2025-11-05 10:00:34 +0100

    _

--- pod-automation/__index.sh
+++ pod-automation/__index.sh
@@ -1,13 +1,13 @@
 # will be imported by the index of the utils
 
-SCRIPT_DIR="$PATH_CLI_UTILS/pod-automation"
+_POD_AUTOMATION_SCRIPT_DIR="$PATH_CLI_UTILS/pod-automation"
 
-source $SCRIPT_DIR/fn-pod-cfr.sh
-source $SCRIPT_DIR/fn-pod-compress.sh
+source $_POD_AUTOMATION_SCRIPT_DIR/fn-pod-cfr.sh
+source $_POD_AUTOMATION_SCRIPT_DIR/fn-pod-compress.sh
 
 
 :pod-thumbnail() {
-    python3 "$SCRIPT_DIR/thumbnail/update-thumbnail.py" "$@"
+    python3 "$_POD_AUTOMATION_SCRIPT_DIR/thumbnail/update-thumbnail.py" "$@"
 }
 
 
